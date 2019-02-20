6b58160710f9dee6bbfa40f16ef7052eb67ed228
JungWoo <qwlake@gmail.com>
2019-02-20 21:46:57 +0900

    Add n11378

--- Python/baekjoon/n11378/n11378.py
+++ Python/baekjoon/n11378/n11378.py
@@ -5,6 +5,8 @@ N, M, K = int(first[0]), int(first[1]), int(first[2])
 possible = []
 possible_count = []
 works_count = [0 for _ in range(M)]
+check = [0 for _ in range(M)]
+
 for i in range(N):
     second = input().split(" ")
     possible_count.append(int(second[0]))
@@ -12,10 +14,26 @@ for i in range(N):
     possible.append(temp)
     for j in temp:
         works_count[j-1] += 1
-
-while True:
-    p_max = possible_count.index(max(possible_count))
-    temp = possible[p_max]
-    sort = sorted(range(len(temp)), key=lambda k: temp[k])
-    for i in sort:
         
+sort = sorted(range(len(possible_count)), key=lambda k: possible_count[k], reverse=True)
+
+for i in sort:
+    p_max = i
+    sort_to_index = sorted(range(len(works_count)), key=lambda k: works_count[k])
+    p_works = sort_to_index[-len(possible[p_max]):]
+    for j in p_works:
+        if check[j] == 0:
+            check[j] = 1
+            if possible_count[p_max] > 0:
+                possible_count[p_max] -= 1
+            elif K > 0:
+                K -= 1
+            else:
+                check[j] = 0
+                break
+                
+count = 0
+for i in check:
+    if i == 1:
+        count += 1
+print(count)
