dae385d0dcbd5d721ce971b084ccdfa3e6bb1bb1
Oh JungWoo <qwlake@gmail.com>
2019-11-05 04:35:54 +0900

    edit settings

--- C/hello.c
+++ C/hello.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    printf("Hello");
-    return 0;
+    int n = 0;
+	scanf("%d", &n);
+	int arr[1000][3];
+	for (int i = 0; i < n; i++) {
+		scanf("%d %d %d", &arr[i][0], &arr[i][1], &arr[i][2]);
+	}
+
+    
+
+	for (int i = 0; i < n; i++) {
+		printf("%d %d %d", arr[i][0], arr[i][1], arr[i][2]);
+	}
+	return 0;
 }
