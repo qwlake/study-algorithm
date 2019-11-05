910c5e42f516b9e0c7fde2bf339a3268e57ffa62
jungwoo <qwlake@gmail.com>
2019-11-05 15:48:24 +0900

    comp n1932

--- C/baekjoon/n1932/n1932.c
+++ C/baekjoon/n1932/n1932.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 
+int max(int a, int b) {return a<b?b:a;}
+
 int main() {
     int n;
 	scanf("%d", &n);
 	
-	long long a[91];
-	a[1] = 1; a[2] = 1;
-	for (int i = 3; i <= n; i++) {
-		a[i] = a[i-1] + a[i-2];
+	int a[500][500];
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j <= i; j++)
+			scanf("%d", &a[i][j]);
+
+	for (int i = 1; i < n; i++) {
+		a[i][0] = a[i][0] + a[i-1][0];
+		for (int j = 1; j <= i+1; j++) {
+			a[i][j] = max(a[i][j]+a[i-1][j], a[i][j]+a[i-1][j-1]);
+		}
 	}
-	printf("%lld", a[n]);
+
+	int max = 0;
+	for (int i = 0; i < n; i++) {
+		max = max < a[n-1][i]? a[n-1][i]:max;
+	}
+	printf("%d", max);
 }
