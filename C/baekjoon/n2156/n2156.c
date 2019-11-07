7b199fb283cb4f2d6d88adc931720f48ccc642f1
jungwoo <qwlake@gmail.com>
2019-11-08 00:10:46 +0900

    comp n2156

--- C/baekjoon/n2156/n2156.c
+++ C/baekjoon/n2156/n2156.c
@@ -1,58 +1,25 @@
 #include <stdio.h>
 
+int n;
+int a[10001];
+int sum[10001];
+
 int max(int a, int b) {return a<b?b:a;}
 
-int possible(int *isDrinked, int idx) {
-	int t1 = isDrinked[idx-1] + isDrinked[idx-2];
-	int t2 = isDrinked[idx+1] + isDrinked[idx+2];
-	if (t1 == 2 || t1 == 2) return 0;
-	else return 1;
+int dp() {
+	sum[0] = a[0] + 0;
+	sum[1] = sum[0] + a[1];
+	for (int i = 2; i < n; i++) {
+		sum[i] = max(max(sum[i-2]+a[i], sum[i-3]+a[i-1]+a[i]), sum[i-1]);
+	}
+	printf("%d", sum[n-1]);
 }
 
 int main() {
-    int n;
 	scanf("%d", &n);
-	int a[10000];
-	int sum[10000] = {0};
-	int isDrinked[10000] = {0};
 	for (int i = 0; i < n; i++) {
 		scanf("%d", &a[i]);
 	}
-
-	sum[0] = a[0]+0;
-	if (n > 1) sum[1] = sum[0] + a[1];
-	if (n > 2) {
-		int x = sum[1]-a[1]+a[2];
-		int y = sum[1]-a[0]+a[2];
-		int m = max(max(sum[1], x), y);
-		if (m == x) {
-			isDrinked[0] = 1;
-			isDrinked[2] = 1;
-		} else if (m == y) {
-			isDrinked[1] = 1;
-			isDrinked[2] = 1;
-		}
-		sum[2] = m;
-	}
-
-	int x,y,z,m,t;
-	for (int i = 3; i < n; i++) {
-		t = isDrinked[i-2];
-		isDrinked[i-2] = 0;
-		x = possible(isDrinked, i-3)? sum[i-1]-a[i-2]+a[i]+a[i-3]:0;
-		isDrinked[i-2] = t;
-		y = sum[i-1]-a[i-3]+a[i];
-		m = max(max(sum[i-1], y), x);
-		if (m == x) {
-			isDrinked[i-2] = 0;
-			isDrinked[i] = 1;
-			isDrinked[i-3] = 1;
-		} else if (m == y) {
-			isDrinked[i-3] = 0;
-			isDrinked[i] = 1;
-		}
-		sum[i] = m;
-	}
-	printf("%d", sum[n-1]);
+	dp();
 	return 0;
 }
