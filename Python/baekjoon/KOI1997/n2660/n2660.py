25a075901f5f788eb1617243ff854eb6517ddd09
Oh JungWoo <qwlake@gmail.com>
2019-12-02 16:50:54 +0900

    comp n2660

--- Python/baekjoon/KOI1997/n2660/n2660.py
+++ Python/baekjoon/KOI1997/n2660/n2660.py
@@ -1,18 +1,33 @@
 import sys
 
 n = int(input())
-a = {i:0 for i in range(1,n+1)}
+a = [[0 for _ in range(n+1)] for _ in range(n+1)]
 
 while True:
     t = list(map(int, sys.stdin.readline().split()))
     if t[0] == -1:
         break
-    a[t[0]] += 1
-    a[t[1]] += 1
+    a[t[0]][t[1]] = 1
+    a[t[1]][t[0]] = 1
 
-sort = sorted(a.items(), key=lambda x: x[1], reverse=True)
-score = n-sort[0][1]
-count = 0
+def find(idx):
+    s = {idx}
+    q = [(idx, 0)]
+    m = 0
+    while len(q) != 0:
+        p, d = q.pop(0)
+        m = d
+        for i, e in enumerate(a[p]):
+            if e == 1 and i not in s:
+                q.append((i, d+1))
+                s.add(i)
+    return m
+
+score_dic = dict()
+for i in range(1,n+1):
+    score_dic[i] = find(i)
+sort = sorted(score_dic.items(), key=lambda x: x[1])
+count = n
 can = ""
 for i, e in enumerate(sort):
     if e[1] != sort[0][1]:
@@ -20,5 +35,5 @@ for i, e in enumerate(sort):
         break
     else:
         can += str(e[0]) + " "
-print(score, count)
+print(sort[0][1], count)
 print(can.strip())
