e8cce75327c4fad3dad5a8557c7229ec66a3c3b5
qwlake <qwlake@gmail.com>
2020-02-29 23:08:28 +0900

    comp n2642

--- C,C++/baekjoon/KOI1999/n2642/n2642.c
+++ C,C++/baekjoon/KOI1999/n2642/n2642.c
@@ -1,12 +1,35 @@
 #include <stdio.h>
 
-int a[8][8] = 0;
-int dir[4][2] = {{-1, 0}, 	// up
-				 {0, 1}, 	// right
-				 {1, 0}, 	// down
-				 {0, -1}};	// left
-int square[3][3] = 0;
-int set[7] = 0;
+int a[8][8] = {0,};
+int d[4][2] = {{-1, 0}, 	// up
+			   {0, 1}, 		// right
+			   {1, 0}, 		// down
+			   {0, -1}};	// left
+int opp[7] = {0,};
+
+void findOpposite(int path[7], int direction, int cnt, int x, int y, int n) {
+	int target;
+	for (int i = 0; i < 4; i++) {
+		target = a[x+d[i][0]][y+d[i][1]];
+		if (x+d[i][0] > 0 && y+d[i][1] > 0 && 
+			x+d[i][0] < 8 && y+d[i][1] < 8 &&
+			target != 0 && 
+			path[target] == 0) {
+			path[target] = 1;
+			if (direction == -1) {
+				findOpposite(path, i, 1, x+d[i][0], y+d[i][1], n);
+			} else if (direction==i && cnt==1 &&
+					   (opp[n]==0 || opp[n]==target) &&
+					   (opp[target]==0 || opp[target]==n)) {
+				opp[n] = target;
+				opp[target] = n;
+				findOpposite(path, direction, cnt+1, x+d[i][0], y+d[i][1], n);
+			} else {
+				findOpposite(path, direction, cnt, x+d[i][0], y+d[i][1], n);
+			}
+		}
+	}
+}
 
 int main() {
 	for (int i = 1; i < 7; i++) {
@@ -14,26 +37,20 @@ int main() {
 			scanf("%d", &a[i][j]);
 		}
 	}
-
-	int posX, posY;
-	for (int i = 0; i < 6; i++) { 	// 1 위치
-		for (int j = 0; j < 6; j++) {
-			printf("%d ", a[i][j]);
-			if (a[i][j] == 1) {
-				posX,posY = i,j;
-				set[1] = 1;
+	for (int i = 1; i < 7; i++) {
+		for (int j = 1; j < 7; j++) {
+			if (a[i][j] != 0) {
+				int path[7] = {0,};
+				path[a[i][j]] = 1;
+				findOpposite(path, -1, 0, i, j, a[i][j]);
 			}
 		}
-		printf("\n");
 	}
-
-	for (int i = 0; i < 4; i++) { 	// 1 주변 4방향 관리
-		if (a[posX+dir[i][0]][posY+dir[i][1]] != 0) {
-			if (square[dir[i][0]+1][dir[i][1]+1] != 0) return;
-			square[dir[i][0]+1][dir[i][1]+1] = a[posX+dir[i][0]][posY+dir[i][1]];
-			set[a[posX+dir[i][0]][posY+dir[i][1]]] = 1;
-		}
+	int flag = 0;
+	for (int i = 1; i < 7; i++) {
+		if (opp[i] == 0) flag = 1;
 	}
-
+	if (flag) printf("%d",0);
+	else printf("%d", opp[1]);
 	return 0;
 }
