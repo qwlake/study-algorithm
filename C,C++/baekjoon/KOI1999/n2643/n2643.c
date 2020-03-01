a26255f153e6ea9cef1a5d9de9d320d81e404ac3
qwlake <qwlake@gmail.com>
2020-03-02 00:29:03 +0900

    comp n2643

--- C,C++/baekjoon/KOI1999/n2643/n2643.c
+++ C,C++/baekjoon/KOI1999/n2643/n2643.c
@@ -1,50 +1,50 @@
 #include <stdio.h>
 
 int n;
-int a[100][2] = {0,};
-int used[100] = {0,};
+int a[100][2];
+int d[100] = {0,};
 
-int dfs(int pre, int cnt) {
-	int ret[100] = {1,};
-	int max = cnt;
-	for (int i = 0; i < n; i++) {
-		if (used[i] == 0 && 
-			((a[pre][0] >= a[i][0] &&
-			a[pre][1] >= a[i][1]) ||
-			(a[pre][0] >= a[i][1] &&
-			a[pre][1] >= a[i][0]))) {
-			used[i] = 1;
-			ret[i] = dfs(i, cnt+1);
-			used[i] = 0;
+void insertionSort(int idx) {
+	int i;
+	int tmp[2] = {a[idx][0], a[idx][1]};
+	for (i = idx-1; i >= 0; i--) {
+		if (a[i][0] > tmp[0]) {
+			a[i+1][0] = a[i][0]; a[i+1][1] = a[i][1];
+		} else if (a[i][0] == tmp[0] && a[i][1] > tmp[1]) {
+			a[i+1][0] = a[i][0]; a[i+1][1] = a[i][1];
+		} else {
+			break;
 		}
 	}
-	for (int i = 0; i < n; i++) {
-		max = max<ret[i]?ret[i]:max;
-	}
-	return max;
+	i++;
+	a[i][0] = tmp[0]; a[i][1] = tmp[1];
 }
 
 int main() {
 	scanf("%d", &n);
+	int t1, t2;
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < 2; j++) {
-			scanf("%d", &a[i][j]);
+		scanf("%d %d", &t1, &t2);
+		if (t1 > t2) {
+			a[i][0] = t2;
+			a[i][1] = t1;
+		} else {
+			a[i][0] = t1;
+			a[i][1] = t2;
+		}
+		insertionSort(i);
+	}
+	for (int i = 0; i < n; i++) {
+		d[i] = 1;
+		for (int j = 0; j < i; j++) {
+			if (a[i][1] >= a[j][1]) {
+				d[i] = d[i]>d[j]+1? d[i]:d[j]+1;
+			}
 		}
 	}
-
-
-
-
-	// int ret[100] = {1,};
-	// int max = 0;
-	// for (int i = 0; i < n; i++) {
-	// 	used[i] = 1;
-	// 	ret[i] = dfs(i, 1);
-	// 	used[i] = 0;
-	// }
-	// for (int i = 0; i < n; i++) {
-	// 	max = max<ret[i]?ret[i]:max;
-	// }
-	// printf("%d", max);
+	for (int i = 1; i < n; i++) {
+		d[0] = d[i]>d[0]? d[i]:d[0];
+	}
+	printf("%d", d[0]);
 	return 0;
 }
