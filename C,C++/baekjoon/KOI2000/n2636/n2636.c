bb2fe2d8feb75578970b14ce70eb6ad2777853a1
qwlake <qwlake@gmail.com>
2020-03-17 22:10:41 +0900

    comp 2636

--- C,C++/baekjoon/KOI2000/n2636/n2636.c
+++ C,C++/baekjoon/KOI2000/n2636/n2636.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
 
-int row, col, tmp, a[102][102]={0};
+int row, col, tmp, a[100][100];
 
-int loop(int x, int y, int notPassed) {
+int loop(int x, int y) {
     if (0<=y-1)
-        if (a[x][y-1]==notPassed) {a[x][y-1]--; loop(x, y-1, notPassed);}
+        if (a[x][y-1]==0) {a[x][y-1]--; loop(x, y-1);}
         else if (a[x][y-1]==1) a[x][y-1]=2;
     if (x+1<row)
-        if (a[x+1][y]==notPassed) {a[x+1][y]--; loop(x+1, y, notPassed);}
+        if (a[x+1][y]==0) {a[x+1][y]--; loop(x+1, y);}
         else if (a[x+1][y]==1) a[x+1][y]=2;
     if (y+1<col)
-        if (a[x][y+1]==notPassed) {a[x][y+1]--; loop(x, y+1, notPassed);}
+        if (a[x][y+1]==0) {a[x][y+1]--; loop(x, y+1);}
         else if (a[x][y+1]==1) a[x][y+1]=2;
     if (0<=x-1)
-        if (a[x-1][y]==notPassed) {a[x-1][y]--; loop(x-1, y, notPassed);}
+        if (a[x-1][y]==0) {a[x-1][y]--; loop(x-1, y);}
         else if (a[x-1][y]==1) a[x-1][y]=2;
 }
-int check(int arg) {
+int check() {
     tmp = 0;
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < col; j++) {
             if (a[i][j] == 2) {
-                a[i][j] = arg;
+                a[i][j] = 0;
                 tmp++;
-            } else if (a[i][j] == 0) {
-                a[i][j] = arg;
+            } else if (a[i][j] == -1) {
+                a[i][j] = 0;
             }
         }
     }
@@ -33,23 +33,21 @@ int check(int arg) {
 
 int main() {
 	scanf("%d %d", &row, &col);
-    row += 2;
-    col += 2;
-    for (int i = 1; i < row-1; i++) {
-        for (int j = 1; j < col-1; j++) {
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
             scanf("%d", &a[i][j]);
         }
     }
-    int arg = 0;
+    int n = 0;
     int cheese = 0;
     int ret = 0;
     while (1) {
-        loop(0, 0, arg);
-        cheese = check(arg-1);
-        arg--;
+        loop(0, 0);
+        cheese = check();
+        n++;
         if (cheese != 0) ret = cheese;
         else break;
     }
-    printf("%d\n%d", -(arg+1), ret);
+    printf("%d\n%d", n-1, ret);
 	return 0;
 }
