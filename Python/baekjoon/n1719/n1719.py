196e5afa22088f338f19e31109048fcf26ac444c
jungwoo-win <qwlake@gmail.com>
2022-06-05 14:04:39 +0900

    edit n1719

--- Python/baekjoon/n1719/n1719.py
+++ Python/baekjoon/n1719/n1719.py
@@ -1,28 +1,29 @@
+import sys
+input = sys.stdin.readline
+
 
 def solution():
     n, m = map(int, input().split())
-    arr = [[[100001, -1] for _ in range(n)] for _ in range(n)]
+    arr = [[200001] * n for _ in range(n)]
+    ans = [['-'] * n for _ in range(n)]
     for _ in range(m):
         x, y, c = map(int, input().split())
-        arr[x - 1][y - 1] = [c, y - 1]
-        arr[y - 1][x - 1] = [c, x - 1]
+        arr[x - 1][y - 1] = arr[y - 1][x - 1] = c
+        ans[x - 1][y - 1] = str(y)
+        ans[y - 1][x - 1] = str(x)
 
     for k in range(n):
         for i in range(n):
-            for j in range(n):
-                t = arr[i][k][0] + arr[k][j][0]
-                if arr[i][j][0] > t:
-                    arr[i][j] = [t, arr[i][k][1]]
+            if i == k:
+                continue
+            for j in range(i + 1, n):
+                t = arr[i][k] + arr[k][j]
+                if arr[i][j] > t:
+                    arr[i][j] = arr[j][i] = t
+                    ans[i][j] = ans[i][k]
+                    ans[j][i] = ans[j][k]
 
-    result = ''
-    for i in range(n):
-        for j in range(n):
-            if i == j:
-                result += '- '
-            else:
-                result += str(arr[i][j][1] + 1) + ' '
-        result += '\n'
-    print(result)
+    print('\n'.join(map(' '.join, ans)))
 
 
 if __name__ == '__main__':
